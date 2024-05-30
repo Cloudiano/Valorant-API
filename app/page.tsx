0be3d786ad22a4d3c7c794e76cb26bff20815a19
Túlio José (Polus) <142807645+Polusv7@users.noreply.github.com>
2024-05-30 10:51:52 -0300

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
       </div>
 
       <Image
-        src="https://media.valorant-api.com/agents/1dbf2edd-4729-0984-3115-daa5eed44993/fullportrait.png"
-        alt="Valorant Clove"
+        src="https://media.valorant-api.com/agents/0e38b510-41a8-5780-5e8f-568b2a4f2d6c/fullportrait.png"
+        alt="Valorant Iso"
         className="animate-pulse"
         width={50}
         height={50}
